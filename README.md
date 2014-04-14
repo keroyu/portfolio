08b824c041e5d8862d28cc55deb11881f7de8d00
Kero Yu <cyudev@gmail.com>
2014-04-15 02:27:07 +0800

    all section done

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ### Kero Yu's Resume
 
 Hi, I'm Kero. I make great websites.
-I was born in Taiwan, but completed most of my education abroad, which makes me quite like an "international" person. I enjoy reading, drawing, playing chess, and of course - coding. Currently I work as a front-end developer for a Taiwanese technology company that focuses on marketing PC/App games.
-
+I was born in Taiwan, but completed most of my education abroad, which makes me quite "international". I enjoy reading, drawing, playing chess, and of course -- coding.
+Currently I work as a front-end developer for a Taiwanese technology company whose main business focuses on marketing PC/App games.
 
 #### Profile
 
@@ -23,7 +23,23 @@ I was born in Taiwan, but completed most of my education abroad, which makes me 
 + Graphics software: Photoshop / Illustrator
 + Solid Knowledge of Information Architecture
 
-#### Works
+### Works
+
+#### Project Manager & Information Architecture
+Intern, Pacific Link Education Centre, Vancouver, Canada
+Developing website usability and structural aesthetics involving SEO concerns based on given contents. Responsible for front-end and back-end scripting. Providing consultant services.
+
+#### Product Director & Website Manager
+Full-time, Sharp Point Publishing Group, Taipei, Taiwan
+Responsible for budget control and project management of Comic Star, an online contest program aiming at talent cultivation. Participating in website concept development, front-end engineering, code revision and quality control. Handling technical issues with contractors. Training staff on copywriting, social networking, customer service and system maintenance.
+
+#### Website Editor
+Contractor, eBay Taiwan, Taipei, Taiwan
+Updating homepage HTML for kijiji.com.tw.
+
+#### Community Planner & Editor
+Full-time, Tom Group, Taipei, Taiwan
+Writing, editing, and formalizing news. Planning events for the community of Video Game Channel. Interviewing foreign producers at international exhibitions.
 
 
 #### Contact
---
+++ js/jq.kerosEasySlider.js
@@ -0,0 +1,155 @@
+(function($){
+    $.fn.kerosEasySlider = function(options){
+
+        var defaults = $.extend({
+                    direction : 'h',
+                    speed: 800,
+                    loop: true
+        }, options );
+
+        return this.each(function(){
+            var o = options;
+
+            var obj = $('.kerosEasySlider'),
+                itemNum = $('.kerosEasySlider ul.item-list li').length,
+                wid = obj.find('.item-list li > *').width(),
+                hgt = obj.find('.item-list li > *').height(),
+                $ul = obj.find('ul.item-list'),
+                $activeLi = obj.find('ul.item-list li[class=active]');
+
+            // IF CHILDREN IS "A", THE WID/HGT MAY BE VOID, THEN
+            if( obj.find('.item-list li').has('a').length ){
+                wid = obj.find('.item-list li > a > *').width();
+                hgt = obj.find('.item-list li > a > *').height();
+                obj.find('.item-list li > a').css({
+                    'display' : 'inline-block', 'width' : wid + 'px', 'height': hgt + 'px'
+                });
+            }
+
+            obj.css( 'height', hgt + 'px' );
+
+            obj.find('.viewport').css({
+                'overflow' : 'hidden', 'width' : wid + 'px', 'height': hgt + 'px'
+            });
+
+            obj.find('.track').css({
+                'position' : 'relative'
+            });
+
+            $ul.css({
+                'position': 'relative', 'text-align' : 'left'
+            });
+
+            obj.find('ul.item-list li').css({
+                'width' : wid + 'px', 'height': hgt + 'px'
+            });
+
+            /* DIRECTION BRANCH  */
+            if( o.direction == 'h' ){
+                var itemUnit = wid,
+                    trackLength = (itemNum*2-1) * itemUnit,
+                    offSetting  = (itemNum-1) * itemUnit;
+
+                obj.find('.track').css({
+                    height : hgt + 'px', width : trackLength + 'px', left  : '-' + offSetting + 'px'
+                });
+
+                // MAKE THE ITEM LIST ALIGN TO THE VIEWPORT
+                $ul.css({
+                    'height' : hgt + 'px', 'left' : offSetting + 'px'
+                })
+
+                obj.find('ul.item-list li').css({
+                    'display' : 'inline-block'
+                })
+
+                // BASIC SLIDER ANIMATION
+                var basicMove = function(thisIndex, targetIndex){
+                    var offset = (targetIndex - thisIndex) * itemUnit;
+                    if( offset > 0 ){
+                        $ul.stop(true,true).animate( { left : "-=" + offset }, o.speed, "easeOutExpo" );
+                    }else{
+                        $ul.stop(true,true).animate({ left :"+="+ Math.abs(offset) }, o.speed, "easeOutExpo" );
+                    }
+                    obj.find('ul.marker li').eq(targetIndex).addClass('active').siblings().removeClass('active');
+                    obj.find('ul.item-list li').eq(targetIndex).addClass('active').siblings().removeClass('active');
+                };
+            }
+
+            if( o.direction == 'v' ){
+                var itemUnit = hgt,
+                    trackLength = (itemNum*2-1) * itemUnit,
+                    offSetting  = (itemNum-1) * itemUnit;
+
+                obj.find('ul.item-list li').css ( 'display', 'block');
+                obj.find('.track').css({
+                    // 'height': itemUnit + 'px',
+                    width : trackLength + 'px',
+                    top  : '-' + offSetting + 'px'
+                });
+
+                // MAKE THE ITEM LIST ALIGN TO THE VIEWPORT
+                $ul.css( 'top', offSetting + 'px' )
+
+                // BASIC SLIDER ANIMATION
+                var basicMove = function(thisIndex, targetIndex){
+                    var offset = (targetIndex - thisIndex) * itemUnit;
+                    if( offset > 0 ){
+                        $ul.stop(true,true).animate( { top : "-=" + offset }, o.speed, "easeOutExpo" );
+                    }else{
+                        $ul.stop(true,true).animate({ top :"+="+ Math.abs(offset) }, o.speed, "easeOutExpo" );
+                    }
+
+                    obj.find('ul.marker li').eq(targetIndex).addClass('active').siblings().removeClass('active');
+                    obj.find('ul.item-list li').eq(targetIndex).addClass('active').siblings().removeClass('active');
+                };
+            }
+
+            /* DIRECTION BRANCH ENDS  */
+
+            // MARKER TRIGGER
+            obj.find('ul.marker li').hover(function(){
+
+                if( $(this).hasClass('active') == false ){
+
+                    var thisIndex = obj.find('ul.marker li[class=active]').index(),
+                        targetIndex = $(this).index();
+                    basicMove(thisIndex, targetIndex);
+                }
+            });
+
+            /* GO TO NEXT OR PREVIOUS */
+            var moveOne = function(whichWay, thisIndex, itemNum){
+                if( whichWay == 'next') {
+                    if( thisIndex == itemNum-1  ){
+                        var targetIndex = o.loop ? 0 : thisIndex;
+                    }else{
+                        var targetIndex = thisIndex + 1;
+                    }
+                }
+                if( whichWay == 'prev') {
+                    if( thisIndex == 0  ){
+                        var targetIndex = o.loop ? itemNum-1 : thisIndex;
+                    }else{
+                        var targetIndex = thisIndex - 1;
+                    }
+                }
+                basicMove(thisIndex, targetIndex);
+            };
+
+            // CONTROLLER
+            obj.find('span.btn a').click(function()
+            {
+                var thisIndex = obj.find('ul.item-list li[class=active]').index();
+                whichWay = $(this).hasClass('next') ? 'next' : 'prev';
+                moveOne( whichWay, thisIndex, itemNum )
+
+            });
+
+
+        }); // FUNCTION DEFINITION ENDS
+
+    }
+
+
+}(jQuery));
