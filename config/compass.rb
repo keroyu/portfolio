788fa60fcc5296c7e24d33a02328f474b75f95f8
Kero Yu <goshinra@gmail.com>
2014-12-13 00:43:41 +0800

    Change css to compact format

--- config/compass.rb
+++ config/compass.rb
@@ -2,11 +2,14 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
+# environment = :production
+environment = :development
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
 javascripts_dir = "js"
+output_style = :compact
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
